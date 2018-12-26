51f1cb265021197a0f0e77c4e544637d574ad746
aransfor <ransfordanthony@gmail.com>
2018-12-26 10:48:08 -0800

    cleaned up labels

--- get_pge_data.py
+++ get_pge_data.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 stock_ticker = 'PCG'
 start_date = '2017-01-01'
-stop_date = '2018-12-01'
+stop_date = '2018-12-24'
 data = pdr.get_data_yahoo(stock_ticker, start_date, stop_date) # the data comes in a panda array type with timestamps and labels
 np_array_stock_data = data.values # this strips it to a array without timestamps and six columns
 highs = np_array_stock_data[:,0]
@@ -18,6 +18,9 @@ ax1.plot(trading_days, opening, label = 'Open value')
 ax1.plot(trading_days, closing, label = 'Close value')
 ax1.plot(trading_days, highs, label = 'Day high')
 ax1.plot(trading_days, lows, label = 'Day low')
-ax2.bar(trading_days, volume)
-plt.legend()
+ax1.set_ylabel('Dollers')
+ax2.bar(trading_days, volume/1e6)
+ax2.set_ylabel('Millions of shares')
+ax1.legend()
+ax2.set_xlabel('Trading days since '+ start_date)
 plt.show()
